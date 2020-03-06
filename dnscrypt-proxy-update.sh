32631147c727e23a71adfe41b569f8bacc9f551d
deen666 <50859409+deen666@users.noreply.github.com>
2020-03-06 17:43:52 +0700

    Create dnscrypt-proxy-update.sh

---
+++ dnscrypt-proxy-update.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+INSTALL_DIR="/opt/dnscrypt-proxy"
+LATEST_URL="https://api.github.com/repos/jedisct1/dnscrypt-proxy/releases/latest"
+PLATFORM="linux"
+CPU_ARCH="x86_64"
+
+Update() {
+    workdir="$(mktemp -d)"
+    curl -sL $(curl -sL "$LATEST_URL" |
+        grep dnscrypt-proxy-${PLATFORM}_${CPU_ARCH}- | grep browser_download_url | head -1 | cut -d \" -f 4) |
+        tar xz -C "$workdir" -f - ${PLATFORM}-${CPU_ARCH}/dnscrypt-proxy &&
+        [ -x ${PLATFORM}-${CPU_ARCH}/dnscrypt-proxy ] &&
+        mv -f "${INSTALL_DIR}/dnscrypt-proxy" "${INSTALL_DIR}/dnscrypt-proxy.old" || : &&
+        mv -f "${workdir}/${PLATFORM}-${CPU_ARCH}/dnscrypt-proxy" "${INSTALL_DIR}/" &&
+        cd "$INSTALL_DIR" && rm -fr "$workdir" &&
+        ./dnscrypt-proxy -check && ./dnscrypt-proxy -service install 2>/dev/null || : &&
+        ./dnscrypt-proxy -service restart || ./dnscrypt-proxy -service start
+}
+
+lversion=$("${INSTALL_DIR}/dnscrypt-proxy" -version)
+rmersion=$(curl -sL "$LATEST_URL" | grep "tag_name" | head -1 | cut -d \" -f 4)
+[ -z "$lversion" ] && exit 1
+[ -z "$rmersion" ] && exit 1
+
+echo locally installed
+echo "$lversion"
+
+echo remote git version
+echo "$rmersion"
+
+if [ "$rmersion" != "$lversion" ]; then
+    echo "Updating" && Update
+else
+    echo "No Update Needed"
+fi
